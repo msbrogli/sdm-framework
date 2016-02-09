3a844720032c5cf909c5ac8d2f33ed4b08f0b905
Marcelo Salhab Brogliato <msbrogli@vialink.com.br>
2016-02-09 05:14:02 -0200

    First steps creating setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='sdm',
+    version='0.0.1',
+    license='GPL',
+    author='Marcelo Salhab Brogliato',
+    author_email='msbrogli@vialink.com.br',
+    description='Sparse Distributed Memory Framework',
+    long_description='',
+    packages=['sdm'],
+    url='https://github.com/msbrogli/sdm-framework',
+)

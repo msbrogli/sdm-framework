8f4b894fde4bac2194b32fef229cfad290e1bb4f
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2018-03-09 17:06:16 -0300

    v1.6.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ libsdm_ext = Extension('sdm._libsdm', [
 ], libraries=libraries, define_macros=macros, extra_compile_args=extra_compile_args, extra_link_args=extra_link_args)
 
 setup(name='sdm',
-    version='1.4.0',
+    version='1.6.0',
     license='GPLv2',
     author='Marcelo Salhab Brogliato',
     author_email='msbrogli@vialink.com.br',

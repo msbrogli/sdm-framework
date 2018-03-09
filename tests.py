a7acee1ea150b025178839a034ee1748e4b9a5af
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2018-03-09 10:29:32 -0300

    Add tests to Bitstring

--- tests.py
+++ tests.py
@@ -3,20 +3,53 @@ from sdm import Bitstring, AddressSpace, Counter, SDM
 import sdm as sdmlib
 from sdm import utils
 
-class SDMTests(unittest.TestCase):
+class BitstringTests(unittest.TestCase):
+    def test_bitstring_init_random(self):
+        bs1 = Bitstring.init_random(1000)
+        cnt1 = bs1.to_binary().count('1')
+        self.assertLessEqual(abs(cnt1 - 500), 100)
 
-    def test_radius_calculation(self):
-        self.assertEqual(451, utils.calculate_radius(1000))
-        self.assertEqual(103, utils.calculate_radius(256))
-        self.assertEqual(4845, utils.calculate_radius(10000))
+    def test_bitstring_init_zeros(self):
+        bs1 = Bitstring.init_zeros(1000)
+        cnt0 = bs1.to_binary().count('0')
+        self.assertEqual(1000, cnt0)
 
-    def test_bitstring(self):
-        bs = Bitstring.init_random(1000)
+    def test_bitstring_init_ones(self):
+        bs1 = Bitstring.init_ones(1000)
+        cnt1 = bs1.to_binary().count('1')
+        self.assertEqual(1000, cnt1)
 
     def test_bitstring_copy(self):
         bs1 = Bitstring.init_random(1000)
         bs2 = bs1.copy()
         self.assertEqual(0, bs1.distance_to(bs2))
+
+    def test_bitstring_flip_random_bits(self):
+        bs1 = Bitstring.init_random(1000)
+        for i in range(1001):
+            bs2 = bs1.copy()
+            bs2.flip_random_bits(i)
+            self.assertEqual(i, bs1.distance_to(bs2))
+
+    def test_bitstring_xor(self):
+        bs1 = Bitstring.init_random(1000)
+
+        bs2 = Bitstring.init_ones(1000)
+        bs3 = bs1.copy()
+        bs3.xor(bs2)
+        self.assertEqual(1000, bs1.distance_to(bs3))
+
+        bs2 = Bitstring.init_zeros(1000)
+        bs3 = bs1.copy()
+        bs3.xor(bs2)
+        self.assertEqual(0, bs1.distance_to(bs3))
+
+
+class SDMTests(unittest.TestCase):
+    def test_radius_calculation(self):
+        self.assertEqual(451, utils.calculate_radius(1000))
+        self.assertEqual(103, utils.calculate_radius(256))
+        self.assertEqual(4845, utils.calculate_radius(10000))
 
     def test_address_space(self):
         as1 = AddressSpace.init_random(1000, 1000000)
